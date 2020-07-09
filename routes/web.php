2fea7575daac22a296b9a52149e0938ae8e7d20a
Marzia Fatarella <marziafatarella@gmail.com>
2020-07-09 16:45:39 +0200

    ok tabella

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/students', 'StudentController'); //crea tutte le rotte in automatico e le nomina già
+//get product => la index
+//create ci restituisce il form per creare un nuovo studente
+//che viene salvato tramite la funzione store
+//show mostra i dettagli
+//delete cancella
+//edit + get restituisce il form per modificare i dati
+//put prende i dati dal form e li salva
+//solo le prime 3 sono navigabili (get). tutte le cose che navighi con il metodo get sono view
